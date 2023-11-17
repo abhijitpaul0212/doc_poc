2e6ac1af86cc133b1dff58577e2ef81e335718c2
Abhijit Paul <abhijitpaul0212@gmail.com>
2023-11-17 21:42:42 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Doc-POC'
+project = 'Project Documentation using Sphinx'
 copyright = '2023, Abhijit'
 author = 'Abhijit'
 release = '1.0.0'
