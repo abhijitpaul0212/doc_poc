0a8f2405509a09c99f8ecedcef8be6f5bc2435d0
Abhijit Paul <abhijitpaul0212@gmail.com>
2023-11-18 02:38:55 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -8,3 +8,17 @@ def print_value(val):
     :return: True
     """
     print(val)
+
+def kill_process(process_name):
+    """
+    This method checks if there is any instance of the application running on Windows OS.
+    If found, then it tries to terminate it
+    :param process_name: name of windows process (type: str)
+    :return: None
+    """
+    tasklist_output = subprocess.check_output(['tasklist']).decode('utf-8')
+    if process_name.lower() in tasklist_output.lower():
+        print("The {0} process is running. Killing it".format(process_name))
+        subprocess.call(['taskkill', '/F', '/IM', process_name])
+    else:
+        print("The {0} process is not running. We will launch a new instance of it".format(process_name))
